78da4a1fb8136a6d3905dc4c4cf374a21bdfe166
Jiupei Zhang <jiupeizhang@gmail.com>
2019-06-15 14:18:27 +0900

    add get PoS module

---
+++ getPosP.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# get pos-word pair
+
+import csv
+
+def get_PosP(filename):
+    docu = list()
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines:
+            if line.startswith("EOS"):
+                continue
+            word, info = line.rstrip().split("\t")
+            info_list = info.split(",")
+            posp = [word, info_list[0]] 
+            docu.append(posp)
+    f.close()
+    return docu
+
+if __name__ == '__main__':
+    data = get_PosP("morphs.txt")
+    with open("posp.csv", "w") as w:
+        writer = csv.writer(w)
+        writer.writerows(data)
+    w.close()
